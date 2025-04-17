cf85db4b32afa819a66e294b54fea5557f5a96cd
Alex-Jay Beukes <alex@skywalkinnovations.co.za>
2025-04-17 14:26:21 +0200

    12

--- Instal-SqlExpres.ps1
+++ Instal-SqlExpres.ps1
@@ -1,13 +1,13 @@
 # PowerShell Script: Install SQL Server 2022 Express Edition
 
 # Variables
-$SqlExpressUrl = "[https://go.microsoft.com/fwlink/?linkid=2204847"](https://go.microsoft.com/fwlink/?linkid=2204847")  # Official SQL Server 2022 Express download
+$SqlExpressUrl = "https://go.microsoft.com/fwlink/?linkid=2204847"  # Official SQL Server 2022 Express download
 $SqlExpressInstaller = "$env:TEMP\SQLServer2022-x64-Express.exe"
 $SaPassword = 'YourStrong!Passw0rd' # <-- CHANGE THIS TO A STRONG PASSWORD
 
-# Download SQL Server Express installer
-Write-Host "Downloading SQL Server 2022 Express installer..."
-Invoke-WebRequest -Uri $SqlExpressUrl -OutFile $SqlExpressInstaller
+# Download SQL Server Express installer using GNU Wget
+Write-Host "Downloading SQL Server 2022 Express installer with wget..."
+wget $SqlExpressUrl -O $SqlExpressInstaller
 
 # Install SQL Server Express silently
 Write-Host "Installing SQL Server 2022 Express..."
--- Install-Git.ps1
+++ Install-Git.ps1
@@ -1,15 +1,12 @@
 # PowerShell Script: Install Git for Windows
 
-# Use TLS 1.2 for secure downloads
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
 # Variables
-$gitUrl = "[https://github.com/git-for-windows/git/releases/latest/download/Git-2.44.0-64-bit.exe"](https://github.com/git-for-windows/git/releases/latest/download/Git-2.44.0-64-bit.exe")
+$gitUrl = "https://github.com/git-for-windows/git/releases/latest/download/Git-2.44.0-64-bit.exe"
 $gitInstaller = "$env:TEMP\Git-2.44.0-64-bit.exe"
 
-# Download Git for Windows installer
-Write-Host "Downloading Git for Windows installer..."
-Invoke-WebRequest -Uri $gitUrl -OutFile $gitInstaller
+# Download Git for Windows installer using GNU Wget
+Write-Host "Downloading Git for Windows installer with wget..."
+wget $gitUrl -O $gitInstaller
 
 # Install Git silently (no user interaction)
 Write-Host "Installing Git for Windows silently..."
