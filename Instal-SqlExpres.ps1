38338dbc587717c74bf7baaa4608324ee070c4a1
Alex-Jay Beukes <alex@skywalkinnovations.co.za>
2025-04-17 14:14:17 +0200

    1

---
+++ Instal-SqlExpres.ps1
@@ -0,0 +1,19 @@
+# PowerShell Script: Install SQL Server 2022 Express Edition
+
+# Variables
+$SqlExpressUrl = "[https://go.microsoft.com/fwlink/?linkid=2204847"](https://go.microsoft.com/fwlink/?linkid=2204847")  # Official SQL Server 2022 Express download
+$SqlExpressInstaller = "$env:TEMP\SQLServer2022-x64-Express.exe"
+$SaPassword = 'YourStrong!Passw0rd' # <-- CHANGE THIS TO A STRONG PASSWORD
+
+# Download SQL Server Express installer
+Write-Host "Downloading SQL Server 2022 Express installer..."
+Invoke-WebRequest -Uri $SqlExpressUrl -OutFile $SqlExpressInstaller
+
+# Install SQL Server Express silently
+Write-Host "Installing SQL Server 2022 Express..."
+$SqlArgs = "/Q /ACTION=Install /FEATURES=SQLEngine /INSTANCENAME=SQLEXPRESS /SECURITYMODE=SQL /SAPWD=`"$SaPassword`" /SQLSVCACCOUNT='NT AUTHORITY\NETWORK SERVICE' /TCPENABLED=1 /IACCEPTSQLSERVERLICENSETERMS"
+Start-Process -FilePath $SqlExpressInstaller -ArgumentList $SqlArgs -Wait -NoNewWindow
+
+# Check SQL Server Express service status
+Write-Host "Checking SQL Server Express service status..."
+Get-Service -Name 'MSSQL$SQLEXPRESS'
